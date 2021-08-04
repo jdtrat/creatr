75844aa544accc48c42fdc112a7d5d0366e1cf0f
Hannah Yoder <yodehs19@wfu.edu>
2021-08-04 15:03:54 -0400

    Add first draft of parsing functions for exit questions

---
+++ R/func_parse-exit.R
@@ -0,0 +1,52 @@
+
+parse_familiarity <- function(.data) {
+
+  # Get answer to "how familiar are you with the following objects?"
+  get_exit_questions(.data = .data) %>%
+    # Filter data to only responses after "FAMILIAR QUESTION ANSWERS:"
+    dplyr::filter(stringr::str_detect(data, "FAMILIAR QUESTION ANSWERS")) %>%
+    dplyr::mutate(data = stringr::str_remove(data, "FAMILIAR QUESTION ANSWERS: "))  %>%
+    # Separate the field column into each object "prompt":
+    tidyr::separate(col = data,
+                    into =  c("chair", "key",
+                              "watch", "brick",
+                              "cup", "knife"),
+                    sep = "&",
+                    extra = "merge",
+                    fill = "right") %>%
+    dplyr::mutate(dplyr::across(.cols = -time,
+                                ~stringr::str_extract(.x, "[:digit:]"))
+    ) %>%
+    tidyr::pivot_longer(cols = -time,
+                        names_to = "object",
+                        values_to = "familiarity_score")
+}
+
+parse_aut <- function(.data) {
+
+  # Get answer to "have you heard of or completed the AUT?"
+  get_exit_questions(.data = .data) %>%
+    # Filter data to find only answer to question (e.g. "ANSWER: YES")
+    dplyr::filter(stringr::str_detect(data, "Answer submitted ")) %>%
+    dplyr::mutate(data = stringr::str_extract(data, "ANSWER:.*"))
+
+}
+
+parse_creativity_def <- function(.data) {
+
+  # Get answer to creativity definition
+  get_exit_questions(.data = .data) %>%
+    # Find all rows where there was a field someone filled out
+    dplyr::filter(stringr::str_detect(data, "FIELD: ")) %>%
+    # Separate the data by spaces, into these columns
+    tidyr::separate(col = data,
+                    into =  c("name_field", "field",
+                              "name_kepyress", "keypress",
+                              "name_value", "value"),
+                    sep = " ",
+                    extra = "merge",
+                    fill = "right") %>%
+    dplyr::slice_tail(n = 1) %>%
+    dplyr::select(time, creativity_def = value)
+
+}
