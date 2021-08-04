53f955ed74b4a13d76c895b942f33b6a753eb103
Jonathan Trattner <accounts@jdtrat.com>
2021-08-04 13:27:54 -0400

    Add utility parsing function for retrieving exit  questions.

---
+++ R/utils_parse.R
@@ -0,0 +1,32 @@
+
+#' Retrieve Exit Questions from CRT
+#'
+#' This function returns the data from the exit questions of the CRT.
+#'
+#' @inheritParams parse_init
+#'
+#' @return A data frame with two columns corresponding to the *exit questions*:
+#' \itemize{
+#' \item{time:} The time since January, 1970
+#' \item{data:} Participant data
+#' }
+#'
+#' @keywords internal
+#'
+#' @examples
+#'
+#' get_exit_questions(crt_data)
+#'
+get_exit_questions <- function(.data) {
+
+  exit_question_start <- .data %>%
+    dplyr::mutate(row = dplyr::row_number()) %>%
+    dplyr::filter(stringr::str_detect(data, 'SHOW: QUESTION')) %>%
+    dplyr::slice_head(n = 1) %>%
+    dplyr::pull(row)
+
+  .data %>%
+    dplyr::slice(exit_question_start:nrow(.data))
+
+}
+
