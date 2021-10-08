12ffba36974fe824c30fb92da9dae85a1fcc4519
Jonathan Trattner <accounts@jdtrat.com>
2021-10-08 12:46:35 -0400

    Delete original parsing functions with the goal of doing SemDis analysis.

--- R/utils_parse.R
+++ R/utils_parse.R
@@ -32,3 +32,20 @@ get_exit_questions <- function(.data) {
 
 }
 
+
+# From jdtools R package
+# https://github.com/jdtrat/jdtools/blob/bd7e7baf7522b8c225b1beb41fea65cad85855fd/R/func_strings-extract.R#L72
+extract_after <- function(full_string, after_string, trim_spaces = TRUE) {
+
+  reg_pattern <- paste0("(?<=", after_string, ").*")
+
+  out <- regmatches(full_string, gregexpr(pattern = reg_pattern,
+                                          text = full_string,
+                                          perl = T))[[1]]
+
+  if (trim_spaces) {
+    out <- trimws(out)
+  }
+
+  return(out)
+}
